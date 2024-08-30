ce9f9307a4f9ae1591310fc716cfb4e966966fcc
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-30 08:20:23 +0530

    Update 11.Binary_Searching.cpp

--- 11.Binary_Searching.cpp
+++ 11.Binary_Searching.cpp
@@ -1,92 +1,51 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<stdlib.h>
-#include<conio.h>
-#define size 20
+#include<conio.h> 
+#define size 20 
+void binary_search();
 void insert();
-void linear_search();
-int arr[size],num,i,n,found=0,pos=-1;
+int c,first,last,middle,n,search,array[100];
 
-int main()
+int main() 
+{ 
+    insert(); 
+    binary_searh();
+    getch(); 
+    return 0; 
+} 
+
+void binary_search() 
 {
-   insert();
-   linear_search();
-   getch();
-	return 0;
-}
+    printf("\n Enter value to find:"); 
+    scanf("%d",&search); 
+    first=0; last=n-1; 
+    middle=(first+last)/2; 
+    while (first<=last) 
+    { 
+         printf("\n.......First element=%d...........Location=%d",array[first], first+1); 
+         printf("\n.......Last element=%d...........Location=%d", array[last],last+1); 
+         printf("\n.......Middle element=%d...........Location=%d",array[middle], middle+1); 
+         printf("\n.........compare %d and %d", array[middle],search); 
+         printf("\n____________________________________________\n"); 
+         getch(); 
+         if(array[middle]<search) 
+                first=middle+1; 
+         else if (array[middle]==search) 
+         { 
+                 printf("\n%d found at location %d.",search,middle+1); 
+                 break; 
+         } 
+         else 
+             last=middle-1; 
+             middle=(first+last)/2; 
+     } 
+     if(first>last) 
+            printf("\nnot found! %d is not present in the list.",search); 
+} 
 
-void linear_search()
-{
-printf("\n Enter the number that has  to be searched:");
-	scanf("%d",&num);
-	for(i=0;i<n;i++)
-   {
-   printf("\n...........compare between %d and %d",arr[i], num);
-		if(arr[i]==num)
-		{
-			found=1;
-			pos=i;
-			printf("\n %d is found in the array at position=%d",num,pos+1);
-			break;
-		}
-	}
-	if(found==0)
-	printf("\n %d does not exist in the array", num);
-}
-
-void insert()
-{
-printf("\n Enter the number of elements in the array:");
-	scanf("%d",&n);
-	printf("\n Enter the elements:");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-}
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-#define size 20
-void insert();
-void linear_search();
-int arr[size],num,i,n,found=0,pos=-1;
-
-int main()
-{
-   insert();
-   linear_search();
-   getch();
-	return 0;
-}
-
-void linear_search()
-{
-printf("\n Enter the number that has  to be searched:");
-	scanf("%d",&num);
-	for(i=0;i<n;i++)
-   {
-   printf("\n...........compare between %d and %d",arr[i], num);
-		if(arr[i]==num)
-		{
-			found=1;
-			pos=i;
-			printf("\n %d is found in the array at position=%d",num,pos+1);
-			break;
-		}
-	}
-	if(found==0)
-	printf("\n %d does not exist in the array", num);
-}
-
-
-
-void insert()
-{
-printf("\n Enter the number of elements in the array:");
-	scanf("%d",&n);
-	printf("\n Enter the elements:");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
+void insert() 
+{ 
+        printf("\n Enter number of elements:"); scanf("%d",&n); 
+        printf("\n enter %d integers:",n); for(c=0;c<n;c++) 
+        scanf("%d",&array[c]); 
 }
